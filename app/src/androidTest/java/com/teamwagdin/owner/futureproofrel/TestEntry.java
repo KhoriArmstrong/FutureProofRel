2f88487198a415f466ea90eb20da3bdd62e23cc4
Khori Armstrong <khoriarmstrong@gmail.com>
2015-04-02 13:39:55 -0400

    Khori Armstrong ~ The start of editing entries; A new activity.

--- app/src/androidTest/java/com/teamwagdin/owner/futureproofrel/TestEntry.java
+++ app/src/androidTest/java/com/teamwagdin/owner/futureproofrel/TestEntry.java
@@ -9,7 +9,13 @@ import junit.framework.TestCase;
  * Created by Owner on 28/01/2015.
  */
 public class TestEntry extends AndroidTestCase {
-    Entry e = new Entry();
+    Entry e;
+
+
+    public void setUp() {
+        e = new Entry();
+    }
+
 
     public void testCreateEntry() {
         assertNotNull(e);
@@ -20,5 +26,20 @@ public class TestEntry extends AndroidTestCase {
         Entry otherEntry = new Entry();
 
         assertTrue( !e.equals(otherEntry) );
+    }
+
+    public void testEntryIsDuplicate() {
+        Entry otherEntry = e.duplicate();
+        //
+        assertTrue( e.resembles(otherEntry) );
+    }
+
+    public void testEntryModified() {
+        Entry oldEntry = e.duplicate();
+        //
+        e.modifyDate(FutureProof.getPresentDateTime());
+        e.modifyMessage("Ayeeeeeeeeeeeeeeee");
+        //
+        assertTrue( !e.resembles(oldEntry) );
     }
 }
--- app/src/main/java/com/teamwagdin/owner/futureproofrel/Entry.java
+++ app/src/main/java/com/teamwagdin/owner/futureproofrel/Entry.java
@@ -4,13 +4,25 @@ package com.teamwagdin.owner.futureproofrel;
  * Created by Owner on 29/01/2015.
  */
 public class Entry {
+    public final int id;
     public String message;
     public EntryDate createDate;
     public EntryDate targetDate;
 
 
 
+    // ??? <-- This IS NOT a permanent solution! This is only to rectify some current problems until Parse is running...
+    private static int _pollNum = 0;
+    private static int poll() {
+        int thisNum = _pollNum;
+        //
+        _pollNum++;
+        //
+        return thisNum;
+    }
+
     public Entry() {
+        id = poll();
         message = "";
         createDate = FutureProof.getPresentDateTime();
         targetDate = FutureProof.getPresentDateTime();
@@ -22,12 +34,72 @@ public class Entry {
         targetDate = thisTarget;
     }
 
-    /*
-    public Entry(String thisMessage, new Date()) {
-        Date Q = new Date(System.currentTimeMillis());
-    }
-    */
 
+
+
+    public void modifyDate(EntryDate thisDate) {
+        if (!thisDate.equals(targetDate)) {
+            targetDate = thisDate;
+        }
+    }
+    public void modifyMessage(String thisString) {
+        if (!thisString.equals(message)) {
+            message = thisString;
+        }
+    }
+
+
+    /**
+     * Creates a soft-clone of the entry
+     * @return
+     */
+    public Entry duplicate() {
+        Entry dup = new Entry();
+        //
+        dup.modifyDate(targetDate);
+        dup.modifyMessage(message);
+        //
+        return dup;
+    }
+
+    /**
+     * Determines if two entries have similar values (but NOT that they are the same!)
+     * @param that
+     * @return
+     */
+    public boolean resembles(Entry that) {
+        if (this.targetDate.equals(that.targetDate)) {
+            if (this.message.equals(that.message)) {
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Entry) {
+            Entry that = (Entry)o;
+            //
+            if (this.id == that.id) {
+                return true;
+            }
+            /*
+            if (this.createDate.equals(that.createDate)) {
+                if (this.targetDate.equals(that.targetDate)) {
+                    if (this.message.equals(that.message)) {
+                        return true;
+                    }
+                }
+            }
+            */
+        }
+
+
+        return false;
+    }
 
     @Override
     public String toString() {
